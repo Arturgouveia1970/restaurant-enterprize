e386ec4ed0abdae670df26c50dd5f571e2d9baa5
Artur <artgouveiag@gmail.com>
2024-02-25 18:47:42 +0100

    created frontend, backend, landing page, login and logout

---
+++ backend/src/index.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+// import mongoose from "mongoose";
+// import myUserRoute from "./routes/MyUserRoute";
+// import { v2 as cloudinary } from "cloudinary";
+// import myRestaurantRoute from "./routes/MyRestaurantRoute";
+// import restaurantRoute from "./routes/RestaurantRoute";
+// import orderRoute from "./routes/OrderRoute";
+
+mongoose
+  const connectionString = process.env.MONGODB_CONNECTION_STRING as string;
+  if (!connectionString) {
+    throw new Error("MONGODB_CONNECTION_STRING is not defined");
+  }
+  mongoose
+    .connect(connectionString) 
+  .then(() => console.log("Connected to database"));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello" });
+});
+
+app.listen(7000, () => {
+  console.log("server started on localhost:7000");
+});
