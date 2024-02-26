14690f0318f60cd7f13e7312a36e9ab1b73bb3fb
Artur <artgouveiag@gmail.com>
2024-02-26 14:06:57 +0100

    authentication complete

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,12 +1,11 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
-// import { jwtCheck, jwtParse } from "../middleware/auth";
-// import { validateMyUserRequest } from "../middleware/validation";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
 // /api/my/user
-router.post("/", MyUserController.createCurrentUser); // Create a new user (log in)
+router.post("/", jwtCheck, MyUserController.createCurrentUser); // Create a new user (log in)
 
 
 export default router;
